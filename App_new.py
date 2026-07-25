9665590b59eedd1a888aa8f9e822cb934ba48971
deepakkhaneja2-debug <deepakkhaneja2@gmail.com>
2026-07-26 01:15:03 +0530

    Update App_new.py

--- App_new.py
+++ App_new.py
@@ -1,1 +1,77 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
 
+st.set_page_config(page_title="AI Stock Screener", layout="wide")
+
+st.title("📈 AI Stock Screener V1")
+st.write("Professional NSE Stock Scanner")
+
+# ==========================
+# NSE Stocks
+# ==========================
+
+stocks = [
+    "RELIANCE.NS",
+    "TCS.NS",
+    "INFY.NS",
+    "HDFCBANK.NS",
+    "ICICIBANK.NS",
+    "SBIN.NS",
+    "LT.NS",
+    "ITC.NS",
+    "BHARTIARTL.NS",
+    "AXISBANK.NS",
+    "KOTAKBANK.NS",
+    "BAJFINANCE.NS",
+    "BAJAJFINSV.NS",
+    "MARUTI.NS",
+    "TITAN.NS",
+    "SUNPHARMA.NS",
+    "ASIANPAINT.NS",
+    "ULTRACEMCO.NS",
+    "NTPC.NS",
+    "POWERGRID.NS",
+    "NESTLEIND.NS",
+    "INDUSINDBK.NS",
+    "WIPRO.NS",
+    "TATAMOTORS.NS",
+    "TATASTEEL.NS",
+    "HINDALCO.NS",
+    "ONGC.NS",
+    "COALINDIA.NS",
+    "ADANIPORTS.NS",
+    "ADANIENT.NS",
+    "GRASIM.NS",
+    "EICHERMOT.NS",
+    "JSWSTEEL.NS",
+    "BPCL.NS",
+    "BRITANNIA.NS",
+    "CIPLA.NS",
+    "DRREDDY.NS",
+    "HEROMOTOCO.NS",
+    "HINDUNILVR.NS",
+    "TECHM.NS"
+]
+
+# ==========================
+# RSI Function
+# ==========================
+
+def calculate_rsi(close, period=14):
+    delta = close.diff()
+
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+
+    avg_gain = gain.rolling(period).mean()
+    avg_loss = loss.rolling(period).mean()
+
+    rs = avg_gain / avg_loss
+
+    rsi = 100 - (100 / (1 + rs))
+
+    return rsi
+
+# Result list
+rows = []
