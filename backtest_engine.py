3540051eed44abddb912c1104950239f290ffa3f
deepakkhaneja2-debug <deepakkhaneja2@gmail.com>
2026-07-31 20:43:17 +0530

    Update backtest_engine.py

--- backtest_engine.py
+++ backtest_engine.py
@@ -1,10 +1,13 @@
 import pandas as pd
 
+from brokerage_engine import BrokerageEngine
+
 
 class BacktestEngine:
 
     def __init__(self):
-
+        self.brokerage = BrokerageEngine()
+        
         # ==============================
         # BACKTEST SETTINGS
         # ==============================
